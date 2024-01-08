60a0ece56352141c4c8ceaac0db52dc6d0bab839
Abimbola <ahassan@chapelhilldenham.com>
2024-01-08 12:30:44 +0100

    fixed the redirect

--- src/app/_shared/dialogs/logout-dialog/logout-dialog.component.ts
+++ src/app/_shared/dialogs/logout-dialog/logout-dialog.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { AuthService } from '@app/_shared/services/auth.service';
+import { environment } from '@environments/environment';
 
 
 @Component({
@@ -12,15 +14,22 @@ export class LogoutDialogComponent implements OnInit {
 
   constructor(private auth: AuthService,
     public _dialogRef: MatDialogRef<LogoutDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private http: HttpClient,
   ) { }
 
   ngOnInit(): void {
   }
 
   logout(): void {
-    this._dialogRef.close();
     this.auth.logout();
+    // this.http.post(`${environment.baseApiUrl}/auth/user/logout`, {},)
+    //   .subscribe((response: any) => {
+    //     this.auth.logout();
+    //     this._dialogRef.close();
+    //   },
+    //   errResp => {
+        this._dialogRef.close();
+      // });
   }
-
 }
